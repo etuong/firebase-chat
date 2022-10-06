0baa50adff057d215f6bb36124b173f36618bf92
etuong <etuong@gmail.com>
2022-10-05 21:03:15 -0700

    Auth

--- src/chat/Header.jsx
+++ src/chat/Header.jsx
@@ -1,9 +1,12 @@
 import React, { memo } from "react";
+import { useAuth } from "../hooks/useAuth";
 
 const Header = () => {
+  const { user } = useAuth();
+
   return (
     <React.Fragment>
-      <b className="name">Test!</b>
+      <b className="name">{user}</b>
     </React.Fragment>
   );
 };
--- src/chat/Welcome.jsx
+++ src/chat/Welcome.jsx
@@ -1,29 +1,41 @@
 import React from "react";
 import Popup from "reactjs-popup";
+import { useAuth } from "../hooks/useAuth";
 
-const Welcome = () => (
-  <Popup open={true} modal>
-    {(close) => (
-      <div className="popup">
-        <button className="close" onClick={close}>
-          &times;
-        </button>
-        <div className="header">Welcome!</div>
-        <div className="content">
-          <div className="welcome-item"></div>
-          <div className="welcome-item">
-            <b>House rules</b>
-            <ul>
-              <li>Be respectful</li>
-              <li>No profanity</li>
-              <li>No nudity</li>
-              <li>Have fun!</li>
-            </ul>
+const Welcome = () => {
+  const { login } = useAuth();
+
+  return (
+    <Popup open={true} modal>
+      {(close) => (
+        <div className="popup">
+          <button className="close" onClick={close}>
+            &times;
+          </button>
+          <div className="header">Welcome!</div>
+          <div className="content">
+            <div className="welcome-item">
+              <h5>Please log in to chat!</h5>
+              <div>
+                <button onClick={login} className="login">
+                  Login with Google
+                </button>
+              </div>
+            </div>
+            <div className="welcome-item">
+              <b>House rules</b>
+              <ul>
+                <li>Be respectful</li>
+                <li>No profanity</li>
+                <li>No nudity</li>
+                <li>Have fun!</li>
+              </ul>
+            </div>
           </div>
         </div>
-      </div>
-    )}
-  </Popup>
-);
+      )}
+    </Popup>
+  );
+};
 
 export default Welcome;
